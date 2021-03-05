78c34f9f3a6752e2a504de064d4b191f2091e5fb
girolle <girolle@github.com>
2021-03-05 13:52:08 +0300

    npm ci

--- 1_sprint/check_port.sh
+++ 1_sprint/check_port.sh
@@ -2,7 +2,7 @@
 . ./tests-second-floor/1_sprint/print_err.sh
 . ./tests-second-floor/1_sprint/ERRORS_KEYS.sh
 
-npm run start &
+npm ci && npm run start &
 sleep 20
 
 PORT=$(netstat -an | grep ':3000 ')
