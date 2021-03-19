ac148a295440db6912a6f199399dcc76cb56a702
girolle <32385353+girolle@users.noreply.github.com>
2021-03-19 18:59:51 +0300

    Update check_github_link.sh

--- 1_sprint/check_github_link.sh
+++ 1_sprint/check_github_link.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+. ./tests-second-floor/1_sprint/print_err.sh
+. ./tests-second-floor/1_sprint/ERRORS_KEYS.sh
 
 LINK=$(cat README.md | grep https://github.com/ | grep /middle.messenger.praktikum.yandex)
 
 if [[ -z $LINK ]]
 then
+	print_err $GITHUB_LINK_ERR
 	exit 1
 fi
 
