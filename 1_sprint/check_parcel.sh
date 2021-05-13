f21691d45438af5f6414d0ba26113ea2f00adcdf
girolle <32385353+girolle@users.noreply.github.com>
2021-05-13 11:12:24 +0300

    Update check_parcel.sh

--- 1_sprint/check_parcel.sh
+++ 1_sprint/check_parcel.sh
@@ -2,11 +2,4 @@
 . ./tests-second-floor/1_sprint/print_err.sh
 . ./tests-second-floor/1_sprint/ERRORS_KEYS.sh
 
-PARCEL=$(npm run | grep "start" -A1 | grep "parcel")
-if [[ -z $PARCEL ]]
-then
-	print_err $PARCEL_ERR
-	exit 1
-fi
-
 exit 0
