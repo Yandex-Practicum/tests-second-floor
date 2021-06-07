55d82905ff5fb563e65c4dfe3de37f580eafa1ca
girolle <32385353+girolle@users.noreply.github.com>
2021-06-07 12:06:01 +0300

    Create check_port.sh

---
+++ selenium_test/check_port.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+. ./tests-second-floor/sprint_1/print_err.sh
+. ./tests-second-floor/sprint_1/ERRORS_KEYS.sh
+
+npm ci && npm run start &
+sleep 40
+
+PORT=$(netstat -an | grep ':3000 ')
+if [[ -z $PORT ]]
+then
+	print_err $PORT_ERR
+	exit 1
+fi
+
+exit 0
