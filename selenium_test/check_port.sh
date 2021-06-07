4f6734b07639d0edd579954445b5b8694193dad1
girolle <32385353+girolle@users.noreply.github.com>
2021-06-07 18:44:22 +0300

    Update check_port.sh

--- selenium_test/check_port.sh
+++ selenium_test/check_port.sh
@@ -2,7 +2,7 @@
 . ./tests-second-floor/sprint_1/print_err.sh
 . ./tests-second-floor/sprint_1/ERRORS_KEYS.sh
 
-npm npm install --no-optional && npm run start &
+npm install --no-optional && npm run start &
 sleep 40
 
 PORT=$(netstat -an | grep ':3000 ')
