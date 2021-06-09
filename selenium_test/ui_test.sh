5b4e93365195da3590efd7105d2792e954a4c875
girolle <32385353+girolle@users.noreply.github.com>
2021-06-09 17:58:46 +0300

    Update ui_test.sh

--- selenium_test/ui_test.sh
+++ selenium_test/ui_test.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+. ./tests-second-floor/sprint_1/print_err.sh
+. ./tests-second-floor/sprint_1/ERRORS_KEYS.sh
+
+
+PORT=$(netstat -an | grep ':3000 ')
+if [[ -z $PORT ]]
+then
+	print_err $PORT_ERR
+	exit 1
+fi
+
+
+
 sudo cp tests-second-floor/geckodriver /usr/local/bin
 npm  --prefix ./tests-second-floor/selenium_test install ./tests-second-floor/selenium_test &> install_errors.txt
 node tests-second-floor/selenium_test/ui_test.js
