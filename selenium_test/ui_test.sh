2c159fbfa2e74b15588484c0d139f64e6ffefc06
girolle <32385353+girolle@users.noreply.github.com>
2021-06-09 17:27:01 +0300

    Update ui_test.sh

--- selenium_test/ui_test.sh
+++ selenium_test/ui_test.sh
@@ -3,4 +3,3 @@ sudo cp tests-second-floor/geckodriver /usr/local/bin
 sudo ls -la /usr/local/bin
 npm  --prefix ./tests-second-floor/selenium_test install ./tests-second-floor/selenium_test &> install_errors.txt
 node tests-second-floor/selenium_test/ui_test.js
-rm geckodriver
