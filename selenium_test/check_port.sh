ea5ea2ba32e59601b0b719cf5957b56e5d07c441
girolle <32385353+girolle@users.noreply.github.com>
2021-06-10 11:05:03 +0300

    Update check_port.sh

--- selenium_test/check_port.sh
+++ selenium_test/check_port.sh
@@ -13,6 +13,9 @@ then
 fi
 
 npm  --prefix ./tests-second-floor/selenium_test install ./tests-second-floor/selenium_test &> install_errors.txt
+npm install -g selenium-webdriver
+npm install -g geckodriver
+d
 node tests-second-floor/selenium_test/ui_test.js
 
 exit 0
