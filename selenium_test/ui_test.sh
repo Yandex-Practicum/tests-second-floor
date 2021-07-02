d5b17d7dcdf1b168eecf96333399af5216160fb3
girolle <32385353+girolle@users.noreply.github.com>
2021-07-02 08:51:30 +0300

    Update ui_test.sh

--- selenium_test/ui_test.sh
+++ selenium_test/ui_test.sh
@@ -4,5 +4,4 @@
 
 npm install -g selenium-webdriver &> install_errors.txt
 npm install -g geckodriver &> install_errors.txt
-npm install -g selenium-webdriver
 node tests-second-floor/selenium_test/ui_test.js
