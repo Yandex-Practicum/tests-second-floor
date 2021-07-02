420601e09f87f39d321dc4a96c6996ee63d0e3d7
girolle <32385353+girolle@users.noreply.github.com>
2021-07-02 08:38:41 +0300

    Update ui_test.sh

--- selenium_test/ui_test.sh
+++ selenium_test/ui_test.sh
@@ -2,10 +2,6 @@
 . ./tests-second-floor/sprint_1/print_err.sh
 . ./tests-second-floor/sprint_1/ERRORS_KEYS.sh
 
-
-netstat -an | grep ':3000 '
-
-
-rm -Rf tests-second-floor/geckodriver 
-npm  --prefix ./tests-second-floor/selenium_test install ./tests-second-floor/selenium_test &> install_errors.txt
+#npm  --prefix ./tests-second-floor/selenium_test install ./tests-second-floor/selenium_test &> install_errors.txt
+npm install -g selenium-webdriver &> install_errors.txt
 node tests-second-floor/selenium_test/ui_test.js
