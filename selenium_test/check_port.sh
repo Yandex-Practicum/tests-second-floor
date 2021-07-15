724c7173cbd336072e5c6aaa4f76ab2028236c6d
girolle <32385353+girolle@users.noreply.github.com>
2021-07-15 15:53:25 +0300

    Update check_port.sh

--- selenium_test/check_port.sh
+++ selenium_test/check_port.sh
@@ -2,7 +2,7 @@
 . ./tests-second-floor/1_sprint/ERRORS_KEYS.sh
 
 npm ci && npm run start &
-timeout 120 bash tests-second-floor/sprint_1/setInterval.sh
+timeout 120 bash tests-second-floor/selenium_test/setInterval.sh
 
 print_err $PORT_ERR
 exit 1
