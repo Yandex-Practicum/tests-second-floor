12ca3f2f8419cfae7fd6f4b970739d007fe742ab
girolle <32385353+girolle@users.noreply.github.com>
2021-07-15 16:23:36 +0300

    Update check_port.sh

--- sprint_1/check_port.sh
+++ sprint_1/check_port.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
-. ./tests-second-floor/sprint_1/print_err.sh
-. ./tests-second-floor/sprint_1/ERRORS_KEYS.sh
+. ./tests-second-floor/1_sprint/print_err.sh
+. ./tests-second-floor/1_sprint/ERRORS_KEYS.sh
 
-npm ci && npm run start &
-sleep 40
+npm install -f && npm run start &
+timeout 100 bash tests-second-floor/sprint_1/setInterval.sh
 
-PORT=$(netstat -an | grep ':3000 ')
-if [[ -z $PORT ]]
-then
-	print_err $PORT_ERR
-	exit 1
+if [[ $? == "0" ]]; then
+	exit 0
 fi
 
-exit 0
+print_err $PORT_ERR
+exit 1
