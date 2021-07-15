8cfce015eefbac808b7a03ee4c5e41909cca52bb
girolle <32385353+girolle@users.noreply.github.com>
2021-07-15 15:49:07 +0300

    Update check_port.sh

--- selenium_test/check_port.sh
+++ selenium_test/check_port.sh
@@ -1,17 +1,8 @@
-#!/bin/bash
-. ./tests-second-floor/sprint_1/print_err.sh
-. ./tests-second-floor/sprint_1/ERRORS_KEYS.sh
+. ./tests-second-floor/1_sprint/print_err.sh
+. ./tests-second-floor/1_sprint/ERRORS_KEYS.sh
 
-npm install -g npm@latest
+npm ci && npm run start &
+timeout 120 bash tests-second-floor/sprint_1/setInterval.sh
 
-npm install -f && npm run start &
-sleep 60
-
-PORT=$(netstat -an | grep ':3000 ')
-if [[ -z $PORT ]]
-then
-	print_err $PORT_ERR
-	exit 1
-fi
-
-exit 0
+print_err $PORT_ERR
+exit 1
