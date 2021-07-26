907d406cecddbeb4720ed2e0a53e3125456bd1b9
girolle <32385353+girolle@users.noreply.github.com>
2021-07-26 10:50:06 +0300

    Update ui_test.js

--- selenium_test/ui_test.js
+++ selenium_test/ui_test.js
@@ -1,67 +1,66 @@
+const webdriver = require("selenium-webdriver");
+const firefox = require("selenium-webdriver/firefox");
+const fs = require("fs");
+const path = require("path");
+const http = require("http");
+const { xPathSignUpSearch, fielfChecks, fieldRef } = require("./initData");
 
-const webdriver = require('selenium-webdriver');
-const firefox = require('selenium-webdriver/firefox');
-let options = new firefox.Options()//.setBinary(firefox.Channel.NIGHTLY);
-options.headless();
-
-
-const fs = require('fs');
-const path = require('path');
-const http = require('http');
-//
-
-function redLog (err) {
-	console.log('\x1b[31m' + err + '\x1b[0m')
+function redLog(err) {
+    console.log(`\x1b[31m${err}\x1b[0m`);
 }
 
+const options = new firefox.Options(); // .setBinary(firefox.Channel.NIGHTLY);
+options.headless();
+
+const delay = (timeout) => new Promise((resolve) => setTimeout(resolve, timeout));
+
 const seleniumTests = {
-    viewCatalog: async (driver, webdriver, errors) => {
+    signUpRoute: async (driver, webdriver, errors) => {
         try {
-	//await driver.navigate.to("")
-
-            await driver.get('http://localhost:3000/');
-	    const src = await driver.getPageSource();
-
-            if (!src.includes('root')) {
-                throw new Error();
+            await driver.get("http://localhost:3000/");
+            const src = await driver.getPageSource();
+            try {
+                const link = await driver.wait(webdriver.until.elementLocated(webdriver.By.xpath(xPathSignUpSearch)), 5000);
+                link.click();
+            } catch (e) {
+                throw Error("NO VALID \"Sign up\"");
             }
-	 const link = await driver.wait(webdriver.until.elementLocated(webdriver.By.css("a[href = '/list']")), 5000);
 
-          const link_text = await link.getText();
-
-          if (link_text !== 'View catalog') {
-            throw new Error();
-          }
-				            throw new Error();
-
-        } catch (e) {
-            errors.push('Должна отрендериться ссылка на /list с текстом View catalog'); //errors.push('test.errors.reactDev.navigation.noLinkViewCatalog'); //Должна отрендериться ссылка на /list с текстом View catalog
+            const url = await driver.getCurrentUrl();
+            if (url !== "http://localhost:3000/sign-up") {
+                throw Error("NO ROUTING TO http://localhost:3000/sign-up");
+            }
+        } catch (err) {
+            errors.push(err.message);
+            return { errors }; // errors.push('test.errors.reactDev.navigation.noLinkViewCatalog');
+            // Должна отрендериться ссылка на /list с текстом View catalog
         }
     },
-      routing: async (driver, webdriver, errors) => {
+    checkRef: async (driver, webdriver, errors) => {
         try {
-            await driver.get('http://localhost:3000/');
-            const src = await driver.getPageSource();
-            if (!src.includes('root')) {
-                throw new Error();
-            }
-			 const link = await driver.wait(webdriver.until.elementLocated(webdriver.By.css("a[href = '/list']")), 5000);
-          link.click()
-          await driver.manage().setTimeouts({ implicit: 2000 });
-                    const loc = await driver.executeScript('return location');
-           if (loc.pathname.toString() !== '/list') {
-                throw new Error(loc.pathname.toString());
-           }
-           const title = await driver.wait(webdriver.until.elementLocated(webdriver.By.css(".country_info__2asaU")), 5000);
-          const title_text = await title.getText();
-          if (title_text !== 'Algeria') {
-            throw new Error();
+            await driver.get("http://localhost:3000/");
+            await driver.getPageSource();
+            const signUpButton = await driver.wait(webdriver.until.elementLocated(webdriver.By.xpath(xPathSignUpSearch)), 5000);
+            signUpButton.click();
+
+            for (const key in fieldRef) {
+                const field = await driver.wait(webdriver.until.elementLocated(webdriver.By.xpath(`.//input[@name='${key}']`)), 5000);
+                field.sendKeys(fieldRef[key]);
             }
 
-        } catch (e) {
-            errors.push('test.errors.reactDev.navigation.wrongRouting'); //При клике на ссылку, адрес страницы должен превратиться в http://localhost/list
+            const create = await driver.wait(webdriver.until.elementLocated(webdriver.By.xpath(xPathSignUpSearch)), 5000);
+            //  create.click();
+            await delay(10000);
+
+            const encodedString = await driver.takeScreenshot();
+            await fs.writeFileSync("./image.png", encodedString, "base64");
+            //   const url = await driver.getCurrentUrl();
+            //  console.log(url)
+        } catch (err) {
+            errors.push(err.message);
+            return { errors };
         }
-    }
+    },
 };
 
 async function test() {
@@ -69,23 +68,23 @@ async function test() {
     let driver = null;
     try {
         driver = new webdriver.Builder()
-            .forBrowser('firefox')
-			.setFirefoxOptions(options)
+            .forBrowser("firefox")
+            .setFirefoxOptions(options)
             .build();
-        for (let key in seleniumTests) {
-            await seleniumTests[key](driver, webdriver, errors);
+        for (const key in seleniumTests) {
+            if (await seleniumTests[key](driver, webdriver, errors)) {
+                break;
+            }
         }
-    } catch (err) { }
+    } catch (err) {
+        console.log(err);
+    } finally {
+        await driver.quit();
+    }
 
-    if (errors.length)
-        redLog(errors)
-    process.exit(errors.length)
+    if (errors.length) { redLog(errors); }
+    process.exit(errors.length);
 }
-
-return test().catch((err) => {
-    return {
-        errors: ['trainer.testSyntaxError']
-    };
-}
-);
-
+test().catch((err) => ({
+    errors: ["trainer.testSyntaxError"],
+}));
