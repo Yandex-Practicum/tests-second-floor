0f73d7a297887d5890d0751eabc50c27d781b9cb
girolle <32385353+girolle@users.noreply.github.com>
2021-08-01 17:50:17 +0300

    Update run.sh

--- sprint_3/run.sh
+++ sprint_3/run.sh
@@ -29,12 +29,28 @@ print_green "CHECK PORT"
 bash tests-second-floor/sprint_1/check_port.sh
 check
 
+print_green "CHECK ROUTING"
+node tests-second-floor/sprint_1/check_routing.js
+check
 
-print_green "CHECK UI"
-pwd
-npm  --prefix ./tests-second-floor/sprint_3 install ./tests-second-floor/sprint_3 &> install_errors.txt
-npm install -g geckodriver &> install_errors.txt
-node tests-second-floor/sprint_3/ui_test.js
+print_green "CHECK TYPESCRIPT"
+node tests-second-floor/sprint_2/check_typescript.js
+check
+
+print_green "CHECK STYLELINT"
+node tests-second-floor/sprint_2/check_stylelint.js
+check
+
+print_green "CHECK ESLINT"
+node tests-second-floor/sprint_2/check_eslint.js
+check
+
+print_green "CHECK MOCHA"
+node tests-second-floor/sprint_3/check_mocha.js
+check
+
+print_green "CHECK CHAI"
+node tests-second-floor/sprint_3/check_chai.js
 check
 
 exit 0
