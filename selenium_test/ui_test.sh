e26dc564941a6bfb8e43384f53dfdf64f142bc46
girolle <32385353+girolle@users.noreply.github.com>
2021-08-20 09:24:12 +0300

    Delete ui_test.sh

--- selenium_test/ui_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pwd
-npm  --prefix ./tests-second-floor/selenium_test install ./tests-second-floor/selenium_test &> install_errors.txt
-
-npm install -g geckodriver &> install_errors.txt
-node tests-second-floor/selenium_test/ui_test.js
