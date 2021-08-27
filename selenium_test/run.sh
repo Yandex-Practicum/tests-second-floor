b0a265435055e777ee23d6cfee11d7cd0f5d64a4
girolle <32385353+girolle@users.noreply.github.com>
2021-08-27 11:38:27 +0300

    Update run.sh

--- selenium_test/run.sh
+++ selenium_test/run.sh
@@ -22,8 +22,11 @@ print_green "CHECK PORT"
 bash tests-second-floor/sprint_1/check_port.sh
 check
 
+start=$(($(date +%s%N)/1000000))
 print_green "CHECK REGISTRATION"
 npm  --prefix ./tests-second-floor/selenium_test install ./tests-second-floor/selenium_test &> install_errors.txt
 npm install -g geckodriver &> install_errors.txt
 node tests-second-floor/selenium_test/ui_test.js
 check
+end=$(($(date +%s%N)/1000000))
+echo "Elapsed Time: $(($end-$start)) milliseconds"
