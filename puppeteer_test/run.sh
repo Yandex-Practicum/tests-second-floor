f76dd651c27a72d39450b89a2245a74741f05e40
girolle <32385353+girolle@users.noreply.github.com>
2021-08-27 11:39:17 +0300

    Update run.sh

--- puppeteer_test/run.sh
+++ puppeteer_test/run.sh
@@ -17,7 +17,10 @@ print_green "CHECK PORT"
 bash tests-second-floor/sprint_1/check_port.sh
 check
 
+start=$(($(date +%s%N)/1000000))
 print_green "CHECK REGISTRATION"
 npm install puppeteer &> install_errors.txt
 node tests-second-floor/puppeteer_test/ui_test.js
 check
+end=$(($(date +%s%N)/1000000))
+echo "Elapsed Time: $(($end-$start)) milliseconds"
