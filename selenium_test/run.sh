8a131903811a132979149211ed97737ce7b6f428
girolle <32385353+girolle@users.noreply.github.com>
2021-08-27 12:17:11 +0300

    Update run.sh

--- selenium_test/run.sh
+++ selenium_test/run.sh
@@ -26,8 +26,8 @@ print_green "CHECK PORT"
 bash tests-second-floor/sprint_1/check_port.sh
 
 
-start=$(($(date +%s%N)/1000000))
 print_green "CHECK REGISTRATION"
+start=$(($(date +%s%N)/1000000))
 npm  --prefix ./tests-second-floor/selenium_test install ./tests-second-floor/selenium_test &> install_errors.txt
 npm install -g geckodriver &> install_errors.txt
 node tests-second-floor/selenium_test/ui_test.js
