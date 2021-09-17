de9cc9f48f9cab3c17abe153491deca35f12e5b7
girolle <32385353+girolle@users.noreply.github.com>
2021-09-17 04:47:36 +0300

    Update check_port.sh

--- sprint_1/check_port.sh
+++ sprint_1/check_port.sh
@@ -1,8 +1,8 @@
 . ./tests-second-floor/sprint_1/print_err.sh
 . ./tests-second-floor/sprint_1/ERRORS_KEYS.sh
 
-pnpm install -f && npm run start &
-timeout 240 bash tests-second-floor/sprint_1/setInterval.sh
+npm install -f && npm run start &
+timeout 480 bash tests-second-floor/sprint_1/setInterval.sh
 
 if [[ $? == "0" ]]; then
 	sleep 15
